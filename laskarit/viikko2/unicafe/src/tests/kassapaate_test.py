326e76dd67db347883c1fab4ec13d3f33ba8af72
juhanikat <juhani.kataja@gmail.com>
2022-03-27 20:02:15 +0300

    viikko2 laskarit tehty

---
+++ laskarit/viikko2/unicafe/src/tests/kassapaate_test.py
@@ -0,0 +1,82 @@
+import unittest
+from kassapaate import Kassapaate
+from maksukortti import Maksukortti
+
+
+class TestKassapaate(unittest.TestCase):
+    def setUp(self):
+        self.kassapaate = Kassapaate()
+        self.maksukortti = Maksukortti(10)
+
+    def test_luodun_kassapaatteen_rahamaara_on_oikea(self):
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+
+    def test_luodun_kassapaatteen_myytyjen_lounaiden_maara_on_oikea(self):
+        self.assertEqual(self.kassapaate.edulliset, 0)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_edullisen_lounaan_ostaminen_riittavalla_kateisella_toimii(self):
+        vaihtoraha = self.kassapaate.syo_edullisesti_kateisella(300)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100240)
+        self.assertEqual(vaihtoraha, 60)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_edullisen_lounaan_ostaminen_ei_riittavalla_kateisella_toimii(self):
+        vaihtoraha = self.kassapaate.syo_edullisesti_kateisella(200)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        self.assertEqual(vaihtoraha, 200)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def test_maukkaan_lounaan_ostaminen_riittavalla_kateisella_toimii(self):
+        vaihtoraha = self.kassapaate.syo_maukkaasti_kateisella(500)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100400)
+        self.assertEqual(vaihtoraha, 100)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_maukkaan_lounaan_ostaminen_ei_riittavalla_kateisella_toimii(self):
+        vaihtoraha = self.kassapaate.syo_maukkaasti_kateisella(200)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
+        self.assertEqual(vaihtoraha, 200)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_edullisen_lounaan_ostaminen_riittavalla_korttisaldolla_toimii(self):
+        maksukortti = Maksukortti(300)
+        palautus = self.kassapaate.syo_edullisesti_kortilla(maksukortti)
+        self.assertEqual(maksukortti.saldo, 60)
+        self.assertEqual(palautus, True)
+        self.assertEqual(self.kassapaate.edulliset, 1)
+
+    def test_edullisen_lounaan_ostaminen_ei_riittavalla_korttisaldolla_toimii(self):
+        maksukortti = Maksukortti(100)
+        palautus = self.kassapaate.syo_edullisesti_kortilla(maksukortti)
+        self.assertEqual(maksukortti.saldo, 100)
+        self.assertEqual(palautus, False)
+        self.assertEqual(self.kassapaate.edulliset, 0)
+
+    def test_maukkaan_lounaan_ostaminen_riittavalla_korttisaldolla_toimii(self):
+        maksukortti = Maksukortti(500)
+        palautus = self.kassapaate.syo_maukkaasti_kortilla(maksukortti)
+        self.assertEqual(maksukortti.saldo, 100)
+        self.assertEqual(palautus, True)
+        self.assertEqual(self.kassapaate.maukkaat, 1)
+
+    def test_maukkaan_lounaan_ostaminen_ei_riittavalla_korttisaldolla_toimii(self):
+        maksukortti = Maksukortti(2)
+        palautus = self.kassapaate.syo_maukkaasti_kortilla(maksukortti)
+        self.assertEqual(maksukortti.saldo, 2)
+        self.assertEqual(palautus, False)
+        self.assertEqual(self.kassapaate.maukkaat, 0)
+
+    def test_kortin_lataaminen_positiivisella_maaralla_toimii(self):
+        maksukortti = Maksukortti(0)
+        self.kassapaate.lataa_rahaa_kortille(maksukortti, 100)
+
+        self.assertEqual(maksukortti.saldo, 100)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100100)
+    
+    def test_kortin_lataaminen_negatiivisella_maaralla_toimii(self):
+        maksukortti = Maksukortti(0)
+        self.kassapaate.lataa_rahaa_kortille(maksukortti, -5)
+
+        self.assertEqual(maksukortti.saldo, 0)
+        self.assertEqual(self.kassapaate.kassassa_rahaa, 100000)
--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,9 +1,36 @@
 import unittest
 from maksukortti import Maksukortti
 
+
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(10)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(15)
+        self.assertEqual(self.maksukortti.saldo, 25)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(5)
+        self.assertEqual(self.maksukortti.saldo, 5)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(15)
+        self.assertEqual(self.maksukortti.saldo, 10)
+
+    def test_metodi_palauttaa_true_jos_rahat_riittavat(self):
+        palautus = self.maksukortti.ota_rahaa(5)
+        self.assertEqual(palautus, True)
+
+    def test_metodi_palauttaa_false_jos_rahat_eivat_riita(self):
+        palautus = self.maksukortti.ota_rahaa(15)
+        self.assertEqual(palautus, False)
+
+    def test_merkkijonon_muodostaminen_toimii_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
