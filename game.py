c5079974ac0604ad6b1fc81480ad47d22fad9468
juhanikat <juhani.kataja@gmail.com>
2022-03-29 15:43:24 +0300

    lisää sanoja sanakirjaan, muokkaa game.py

--- game.py
+++ game.py
@@ -1,21 +1,29 @@
 from lxml import etree
 import random
 
-with open("./sanakirja.xml") as xml:
-    tree = etree.parse(xml)
+
+def main():
+    with open("./sanakirja.xml") as xml:
+        tree = etree.parse(xml)
+        
     dictionary = tree.getroot()
-    rnd = dictionary[random.randint(0, len(dictionary)-1)]
-    word = rnd[0].text.strip()
-    print()
-    for defn in rnd[1]:
-        print(defn.text)
-    print()
     while True:
-        answer = input("(Input q to quit) The word is: ").strip().lower()
-        if answer == 'q':
-            break
-        if answer == word:
-            print("Correct!")
-            break
-        else:
-            print("Nope")
+        item = dictionary[random.randint(0, len(dictionary) - 1)]
+        word = item[0].text.strip()
+        print('----------------')
+        for category in item[1]:
+            print(f"{category.tag.capitalize()}:")
+            for defn in category:
+                print(defn.text)
+        print('----------------')
+        while True:
+            answer = input("(Input q to quit) The word is: ").strip().lower()
+            if answer == "q":
+                return
+            if answer == word:
+                print(f"Correct! The word was {word}.")
+                break
+            else:
+                print("Nope, try again.")
+if __name__ == '__main__':
+    main()
